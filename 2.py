35d5f8a99bea9a9e291be6042d1dcebfa1b2787f
Maksik10 <165254740+Maksik10@users.noreply.github.com>
2024-04-04 17:37:45 +1000

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+pygame.init()
+screen_width = 800
+screen_height = 800
+title = "Пример"
+background_color = (68, 255, 0)
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption(title)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill(background_color)
+    pygame.display.flip()
